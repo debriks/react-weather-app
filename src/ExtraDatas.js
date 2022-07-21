88d54118bf8936467945819aa08cd8d9d8f83216
Debriks <deborah.robbiano@gmail.com>
2022-07-21 20:24:19 +0200

    Updated ExtraDatas.js

---
+++ src/ExtraDatas.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./App.css";
+import "./ExtraDatas.css";
+
+export default function ExtraDatas() {
+  let extraDatas = {
+    high: 35,
+    low: 22,
+    humidity: 0,
+    wind: 4,
+  };
+  return (
+    <div className="ExtraDatas d-flex flex-row">
+      <div className="HighLow">
+        Highest <strong>{extraDatas.high}°</strong> <br />
+        Lowest <strong>{extraDatas.low}°</strong> <br />
+      </div>
+      <div className="HumidityWind">
+        Humidity <strong>{extraDatas.humidity} %</strong>
+        <br />
+        Wind <strong>{extraDatas.wind} km/h </strong>
+      </div>
+    </div>
+  );
+}
